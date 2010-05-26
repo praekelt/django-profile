db138a21a444482b9c5fa68144367639353dcbe3
shaunsephton <shaunsephton@gmail.com>
2010-05-26 15:39:43 +0200

    inherit from imagemodel

--- profile/models.py
+++ profile/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Profile(models.Model):
+from photologue.models import ImageModel
+
+class Profile(ImageModel):
     user = models.ForeignKey(
         User, 
         unique=True
