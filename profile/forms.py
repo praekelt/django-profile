5b664269329f823b0c10f6454bc05ae4fcaa306b
shaunsephton <shaunsephton@gmail.com>
2010-05-28 16:04:02 +0200

    beter label

--- profile/forms.py
+++ profile/forms.py
@@ -2,4 +2,6 @@ from registration.forms import RegistrationFormTermsOfService
 from captcha.fields import ReCaptchaField
 
 class RegistrationForm(RegistrationFormTermsOfService):
-    recaptcha = ReCaptchaField()
+    recaptcha = ReCaptchaField(
+        label="Human?",
+    )
