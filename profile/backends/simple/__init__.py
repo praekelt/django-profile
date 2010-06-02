e007f1459f429cc83d9471ceefba715edad5932e
shaunsephton <shaunsephton@gmail.com>
2010-06-02 09:33:48 +0200

    implimented

--- profile/backends/simple/__init__.py
+++ profile/backends/simple/__init__.py
@@ -1,7 +1,27 @@
-from registration.backends.default import DefaultBackend
+from registration.backends.simple import SimpleBackend
+from django.contrib import messages
+from django.utils.translation import ugettext
 
-from profile.forms import RegistrationForm
+from registration import signals
 
-class DefaultBackend(DefaultBackend):
+from profile import utils
+
+class SimpleBackend(SimpleBackend):
     def get_form_class(self, request):
-        return RegistrationForm
+        return utils.get_profile_model().registration_form
+
+def user_registered(sender, user, request, *args, **kwargs):
+    profile = user.profile
+    for field in request.POST:
+        if hasattr(user, field):
+            setattr(user, field, request.POST.get(field))
+        if hasattr(profile, field):
+            setattr(profile, field, request.POST.get(field))
+
+    user.save()
+    profile.save()
+
+    msg = ugettext("You have signed up successfully.")
+    messages.success(request, msg, fail_silently=True)
+
+signals.user_registered.connect(user_registered)
