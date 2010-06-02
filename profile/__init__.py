d7780f645268abe7f8531209587cb89f08df554b
shaunsephton <shaunsephton@gmail.com>
2010-06-02 09:34:37 +0200

    add profile to user

---
+++ profile/__init__.py
@@ -0,0 +1,15 @@
+from django.db.models.signals import class_prepared
+from django.contrib.auth.models import SiteProfileNotAvailable
+
+# connect profile property to User model  
+def connect_profile(sender, **kwargs):
+    from profile import utils
+    try:
+        profile_model = utils.get_profile_model()
+    except SiteProfileNotAvailable:
+        profile_model = None
+    if profile_model:
+        from django.contrib.auth.models import User
+        User.profile = property(lambda u: profile_model.objects.get_or_create(user=u)[0])
+
+class_prepared.connect(connect_profile,)
