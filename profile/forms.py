aa4c2782cf3b416845780c2a319314b8331b2edf
shaunsephton <shaunsephton@gmail.com>
2010-06-02 09:32:47 +0200

    model forms

--- profile/forms.py
+++ profile/forms.py
@@ -1,7 +1,60 @@
-from registration.forms import RegistrationFormTermsOfService
-from captcha.fields import ReCaptchaField
+from django import forms
+from django.forms import ModelForm
 
-class RegistrationForm(RegistrationFormTermsOfService):
-    recaptcha = ReCaptchaField(
-        label="Human?",
-    )
+from profile.models import AbstractAvatarProfile, AbstractProfileBase, AbstractLocationProfile, AbstractContactProfile, AbstractPersonalProfile, AbstractSubscriptionProfile
+
+from registration.forms import RegistrationForm
+
+class SubscriptionsForm(ModelForm):
+    class Meta:
+        model = AbstractSubscriptionProfile
+
+class RegistrationFormSubscriptions(RegistrationForm):
+    def __init__(self, *args, **kwargs):
+        super(RegistrationFormSubscriptions, self).__init__(*args, **kwargs)
+        self.fields.update(SubscriptionsForm().fields)
+
+class LocationModelForm(ModelForm):
+    class Meta:
+        model = AbstractLocationProfile
+
+class LocationForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super(LocationForm, self).__init__(*args, **kwargs)
+        self.fields.update(LocationModelForm().fields)
+
+class PersonalModelForm(ModelForm):
+    class Meta:
+        model = AbstractPersonalProfile
+
+class PersonalForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super(PersonalForm, self).__init__(*args, **kwargs)
+        self.fields.update(PersonalModelForm().fields)
+
+class ContactModelForm(ModelForm):
+    class Meta:
+        model = AbstractContactProfile
+
+class ContactForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super(ContactForm, self).__init__(*args, **kwargs)
+        self.fields.update(ContactModelForm().fields)
+
+class ProfileBaseModelForm(ModelForm):
+    class Meta:
+        model = AbstractProfileBase
+
+class ProfileBaseForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super(ProfileBaseForm, self).__init__(*args, **kwargs)
+        self.fields.update(ProfileBaseModelForm().fields)
+
+class AvatarModelForm(ModelForm):
+    class Meta:
+        model = AbstractAvatarProfile
+
+class AvatarForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super(AvatarForm, self).__init__(*args, **kwargs)
+        self.fields.update(AvatarModelForm().fields)
