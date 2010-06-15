eb55a10d5dd9c45241acc389b67823bc272bf241
shaunsephton <shaunsephton@gmail.com>
2010-06-15 16:19:51 +0200

    added photologue dep

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     url='http://github.com/praekelt/django-profile',
     packages = find_packages(),
     dependency_links = [
-        'https://github.com/praekelt/django-photologue/tarball/master#egg=django-photologue',
+        'http://github.com/praekelt/django-photologue/tarball/master#egg=django-photologue',
     ],
     install_requires = [
         'django-photologue',
