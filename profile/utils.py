e02df44349e6883679d494d064006ccd25c23674
shaunsephton <shaunsephton@gmail.com>
2010-06-25 11:26:06 +0200

    moved check to init

--- profile/utils.py
+++ profile/utils.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.contrib.auth.models import SiteProfileNotAvailable
 
 def get_profile_model():
     auth_profile_module = getattr(settings, 'AUTH_PROFILE_MODULE', None)
@@ -9,7 +8,4 @@ def get_profile_model():
         app_label, model = auth_profile_module.split('.')
         profile_model = getattr(__import__("%s.models" % app_label, globals(), locals(), [model,], -1), model, None)
 
-    if profile_model:
-        return profile_model
-    else:
-        raise SiteProfileNotAvailable
+    return profile_model
