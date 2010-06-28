a39a08d132545ef84d0bb3226b55393fb7e21080
shaunsephton <shaunsephton@gmail.com>
2010-06-28 05:49:24 +0200

    redirect to next

--- profile/backends/simple/__init__.py
+++ profile/backends/simple/__init__.py
@@ -1,14 +1,35 @@
-from registration.backends.simple import SimpleBackend
 from django.contrib import messages
 from django.utils.translation import ugettext
 
 from registration import signals
+from registration.backends.simple import SimpleBackend
 
 from profile import utils
 
 class SimpleBackend(SimpleBackend):
     def get_form_class(self, request):
         return utils.get_profile_model().registration_form
+
+    def post_registration_redirect(self, request, user):
+        """
+        After registration, redirect to the next or otherwise user's absolute url.   
+        """
+        return (self._get_redirect_url(request), (), {})
+    
+    def _get_redirect_url(self, request):
+        """
+        Next gathered from sesion, then GET, then POST, then users absolute url.
+        """
+        if 'next' in request.session:
+            next = request.session['next']
+            del request.session['next']
+            return next
+        elif 'next' in request.GET:
+            return request.GET.get('next')
+        elif 'next' in request.POST:
+            return request.POST.get('next')
+        else:
+            return user.get_absolute_url()
 
 def user_registered(sender, user, request, *args, **kwargs):
     profile = user.profile
