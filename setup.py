0c9374d7102300d6f89146e49b897424f523bde4
shaunsephton <shaunsephton@gmail.com>
2010-07-16 12:18:22 +0200

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-profile',
-    version='0.0.1',
+    version='0.0.2',
     description='Django user profile app.',
     long_description = open('README.rst', 'r').read(),
     author='Praekelt Foundation',
